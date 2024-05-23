985f2374b50bfd9f646d33be32e63a123e04fe1d
dinopegassi <nmbost@gmail.com>
2024-05-23 10:33:50 +0930

    borrado marker.js - entendido e.target.getbounds

--- newer_app/src/comps/EventoClick.js
+++ newer_app/src/comps/EventoClick.js
@@ -3,7 +3,14 @@ import { useMapEvents } from "react-leaflet";
 const EventoClick = () => {
   useMapEvents({
     click: (e) => {
-      console.log(JSON.stringify(e.latlng));
+      console.log('latlng '+JSON.stringify(e.latlng));
+      console.log('containerPoint '+JSON.stringify(e.containerPoint));
+      console.log('layerPoint '+JSON.stringify(e.layerPoint));
+      console.log('target.getCenter() '+JSON.stringify(e.target.getCenter()));
+      console.log('target.getBounds() '+JSON.stringify(e.target.getBounds()));
+      console.log('type '+JSON.stringify(e.type));
+      console.log('originalEvent '+JSON.stringify(e.originalEvent));
+      console.log('popup '+JSON.stringify(e.popup));
     },
   });
   return null;
--- newer_app/src/comps/Map.js
+++ newer_app/src/comps/Map.js
@@ -1,5 +1,4 @@
 import { MapContainer, TileLayer, WMSTileLayer } from "react-leaflet";
-import Mark from "./Marker";
 import preferencias from "../settings/preferences.json";
 import EventoClick from "./EventoClick";
 
@@ -7,7 +6,6 @@ const Map = () => {
   return (
     <MapContainer {...preferencias.preferences}>
       <TileLayer {...preferencias.basemap} />
-      <Mark position={[51.505, -0.09]} />
       {/* datos obtenidos de https://wms.ign.gob.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities */}
       <WMSTileLayer {...preferencias.layers} />
       <EventoClick />
